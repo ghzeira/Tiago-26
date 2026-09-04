3aa4787ceccbe594f04fef4591318764b7dc4d47
ghzeira <gustavo.dosreis432@gmail.com>
2026-09-03 22:28:48 -0300

    Delete cadastro.ts

--- cadastro.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cadastro',
-  standalone: false,
-  templateUrl: './cadastro.html',
-  styleUrl: './cadastro.css',
-})
-export class Cadastro {}
