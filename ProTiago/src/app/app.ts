d3519105396abf332a1a385f810a404e715fbae4
ghzeira <gustavo.dosreis432@gmail.com>
2026-09-09 00:40:57 +0000

    Exercico 1

--- ProTiago/src/app/app.ts
+++ ProTiago/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,9 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ProTiago');
+
+  nome = 'Carlos';
+  idade = 25;
+  curso = 'Sistemas de Informação';
+
 }
